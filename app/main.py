a820a3403fb18c904b8e2f86250d094d36456e03
Fredde <welandfr@arcada.fi>
2026-04-12 18:10:18 +0300

    lektion 1-3

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
-from app.db import get_conn, create_schema
 
 app = FastAPI()
 
@@ -15,33 +14,19 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Skapa databas-schema:
-create_schema()
-
-# tillfällig "databas"
-temp_rooms = [
-    { "room_number": 101, "room_type": "Double room", "price": 100},
-    { "room_number": 202, "room_type": "Single room", "price": 80},
-    { "room_number": 303, "room_type": "Suite", "price": 500}
-]
-
 @app.get("/")
 def read_root():
-    # testa databasen
-    with get_conn() as conn, conn.cursor() as cur:
-        cur.execute("""
-            SELECT 
-                'databasen funkar!' AS msg, 
-                version() AS version
-        """)
-        db_status = cur.fetchone()
-    return { "msg": "Välkommen till hotellets boknings-API", "db": db_status}
+    return { "msg": "Hello local docker"}
 
-@app.get("/rooms")
-def rooms():
-    return temp_rooms
+@app.get("/api/ip")
+def ip(request: Request):
+    return { "ip": request.client.host}
 
-@app.post("/bookings")
-def create_booking():
-    # skapa bokningen i databasen, INSERT INTO bookings...
-    return { "msg": "Bokning skapad!"}
+
+@app.get("/ip", response_class=HTMLResponse)
+def ip(request: Request):
+    return f"<h1>Din ip är {request.client.host}</h1>"
+
+@app.get("/hello")
+def hello():
+    return { "msg": "Hello fredde"}
